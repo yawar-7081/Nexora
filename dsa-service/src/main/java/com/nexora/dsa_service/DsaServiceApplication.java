381f87437b2e00ec712bb5fc41dc49e835168e89
Yawar <mdyawarrkt@gmail.com>
2026-04-26 11:32:56 +0530

    Initial Commit - Added Base Structure

---
+++ dsa-service/src/main/java/com/nexora/dsa_service/DsaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nexora.dsa_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaServiceApplication.class, args);
+	}
+
+}
---
+++ dsa-service/src/test/java/com/nexora/dsa_service/DsaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexora.dsa_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
