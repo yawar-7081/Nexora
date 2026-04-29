9fb5dd37c37bd6106ddbe2016b8205a22b496f90
Yawar <mdyawarrkt@gmail.com>
2026-04-29 11:34:04 +0530

    Added 2 repository ExecutionResultRepository, SubmissionRepository

--- dsa-service/src/main/java/com/nexora/dsa_service/repository/ExecutionResultRepository.java
+++ dsa-service/src/main/java/com/nexora/dsa_service/repository/ExecutionResultRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExecutionResultRepository extends JpaRepository<ExecutionResult,String> {
+    void deleteBySubmissionId(String id);
 }
--- dsa-service/src/main/java/com/nexora/dsa_service/repository/SubmissionRepository.java
+++ dsa-service/src/main/java/com/nexora/dsa_service/repository/SubmissionRepository.java
@@ -4,6 +4,12 @@ import com.nexora.dsa_service.entity.Submission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SubmissionRepository extends JpaRepository<Submission, String> {
+    boolean existsByProblemId(String problemId);
+    void deleteByProblemId(String problemId);
+
+    List<Submission> findByProblemId(String problemId);
 }
